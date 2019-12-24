96a74f80a88b3790a3a7324f1303c6054b745253
moon-grp <olumidemm@gmail.com>
2019-12-24 21:13:07 +0100

    /home ready

---
+++ src/components/animation.vue
@@ -0,0 +1,208 @@
+<template>
+<div>
+    <div class="row q-mt-sm">
+      <div class="heart"></div>
+      <div class="decagon"></div>
+      <div class="rabbet"></div>
+      <div class="heart"></div>
+      <div class="square"></div>
+    </div>
+    <div class="row pushert">
+      <div class="eclipse q-ml-xl"></div>
+      <div id="pacman"></div>
+      <div class="lol_arr"></div>
+      <div class="nonagon"></div>
+      <div class="circle_ish"></div>
+    </div>
+    <div class="row pushert">
+       <div id="pacman"></div>
+      <div class="heart"></div>
+      <div class="circle_ish"></div>
+     <div class="rabbet"></div>
+    </div>
+    <div class="row q-mt-sm">
+      <div class="heart"></div>
+      <div class="decagon"></div>
+      <div class="rabbet"></div>
+      <div class="heart"></div>
+      <div class="square"></div>
+    </div>
+
+  <!--  <div class="row">
+      <div class="square"></div>
+    <div class="circle_ish"></div>
+    <div class="eclipse"></div>
+    <div class="decagon"></div>
+     <div class="nonagon"></div>
+     <div class="rabbet"></div>
+     <div class="lol_arr"></div>
+    </div> -->
+
+</div>
+    
+</template>
+
+<script>
+import anime from "animejs";
+export default {
+    name:'animation',
+    data() {
+        return {
+            
+        }
+    },
+     methods: {
+  animatesircles() {
+      anime({
+        targets: "#pacman",
+        easing: "linear",
+        delay: 800,
+        duration:2000,
+       
+        loop:true
+      }); 
+
+        anime({
+        targets: ".blockk",
+        backgroundColor:"#6c6c6c",
+        easing: "linear",
+        delay: 800,
+        duration:200,
+        translateY: "45%",
+      }); 
+     
+    }
+  },
+     mounted(){
+    this.animatesircles()
+  }
+    
+}
+</script>
+
+
+<style scoped>
+
+.pushert{
+  margin-left: 20%;
+}
+
+.blockk {
+    clip-path: polygon(50% 0%, 100% 64%, 100% 89%);
+    background-color: blueviolet;
+    width: 200px;
+  height: 200px;
+}
+
+.square{
+    clip-path: polygon(0 0, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0);
+    background-color: #F9F871;
+    width: 40px;
+    height: 40px;
+  
+
+}
+
+.circle_ish{
+    clip-path: circle(76.1% at 50% 100%);
+     background-color: #FFD767;
+    width: 100px;
+    height: 100px;
+}
+
+.eclipse{
+    clip-path: ellipse(16% 40% at 50% 52%);
+     background-color: #EC87E4;
+    width: 50px;
+    height: 50px;
+}
+
+.decagon{
+    clip-path: polygon(50% 0%, 80% 10%, 100% 35%, 100% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%);
+      background-color: #FFB47A;
+    width: 100px;
+    height: 100px;
+}
+
+.nonagon{
+    clip-path: polygon(50% 0%, 83% 12%, 100% 43%, 94% 78%, 68% 100%, 32% 100%, 6% 78%, 0% 43%, 17% 12%);
+     background-color: #FF87C2;
+    width: 100px;
+    height: 100px;
+}
+
+.rabbet{
+    clip-path: polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);
+      background-color: #FF979C;
+    width: 80px;
+    height: 80px;
+}
+
+.lol_arr{
+    clip-path: polygon(50% 89%, 100% 0, 55% 0, 55% 100%, 100% 49%, 0 49%, 44% 100%, 45% 0, 0 0);
+       background-color:#9C3C97;
+    width: 100px;
+    height: 100px;
+}
+
+#pacman {
+      width: 0px;
+      height: 0px;
+      border-right: 60px solid transparent;
+      border-top: 60px solid red;
+      border-left: 60px solid red;
+      border-bottom: 60px solid red;
+      border-top-left-radius: 60px;
+      border-top-right-radius: 60px;
+      border-bottom-left-radius: 60px;
+      border-bottom-right-radius: 60px;
+    }
+
+.heart {
+  position: relative;
+  
+  width: 200px;
+  height: 160px;
+}
+.heart:before,
+.heart:after {
+  position: absolute;
+  content: "";
+  
+  width: 100px;
+  height: 160px;
+  left: 100px;
+  
+  background-color: tomato;
+  
+  border-radius: 100px 100px 0 0;
+  
+  transform: rotate(-45deg);
+  transform-origin: 0 100%;
+}
+.triangle {
+  width: 0;
+  height: 0;
+}
+
+
+.triangle .triangle-top-left {
+  border-top: 100px solid #32557f;
+  border-right: 100px solid transparent;
+}
+
+.triangle .triangle-top-right {
+  border-top: 100px solid #32557f;
+  border-left: 100px solid transparent;
+}
+
+.triangle .triangle-bottom-left {
+  border-bottom: 100px solid #32557f;
+  border-right: 100px solid transparent;
+}
+
+.triangle .triangle-bottom-right {
+  border-bottom: 100px solid #32557f;
+  border-left: 100px solid transparent;
+}
+</style>
---
+++ src/layouts/home.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="row">
+    <div class="col q-mt-xl q-ml-xl">
+      <!--   <div class="q-mt-xl">
+                  <q-avatar size="40px" >
+      <img  src="~assets/mimo.png">
+    </q-avatar>
+              </div> -->
+      <div class="q-mt-sm">
+        <span class="font_nav">
+          About
+        </span>
+      </div>
+         <div class="q-mt-sm" ref="contact">
+                <span class="font_nav">
+              Experience
+            </span>
+              </div> 
+      <div class="q-mt-sm q-mb-xl">
+        <span class="font_nav">
+          Projects
+        </span>
+      </div>
+      <div class="q-mt-xl pusherT">
+        <span class="font_name">
+          Olumide Adebayo.
+        </span>
+           <div class="font_style">
+      <span>    I have over three years of experience building responsive and accessible websites </span>
+      <span> I primarily work with ReactJS.</span>
+      </div>
+      <div class="q-gutter-sm q-mt-sm">
+      <q-icon name="fab fa-github" style="color: #ccc;" size="35px"/> 
+       <q-icon name="fab fa-twitter" style="color: #ccc;" size="35px"/> 
+        <q-icon name="fab fa-linkedin" style="color: #ccc;" size="35px"/> 
+      </div>
+      </div>
+   
+      <div>
+        <q-btn
+          elevated
+          size="lg"
+          style="background: #BFFCF9;"
+          label="send me a message"
+          class="shadow-7 q-mt-md btn_font"
+        />
+      </div>
+    </div>
+    <div class="col">
+      <!--   <animation /> -->
+      <div class="block"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import animation from "../components/animation"
+export default {
+  name: "home",
+  components: {
+    //animation
+  },
+  data() {
+    return {
+      value: 81
+    };
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Baloo+Bhai|Oswald:300,400&display=swap");
+.font_nav {
+  font-family: "Oswald", sans-serif;
+  font-size: 25px;
+  color: #ec87e4;
+}
+.font_name {
+  font-family: "Baloo Bhai", cursive;
+  font-size: 47px;
+  color: #ec87e4;
+  font-weight: 400;
+}
+
+.pusherT {
+  margin-top: 15%;
+}
+
+.font_style{
+   font-family: "Oswald", sans-serif;
+   font-size: 20px;
+   color: #ec87e4;
+}
+
+.btn_font{
+   font-family: "Oswald", sans-serif;
+   font-size: 60px;
+   font-weight: bold;
+   
+}
+
+body {
+  height: 100vh;
+  border-style: double;
+  border-color: #bffcf9;
+  border-width: 5px;
+}
+</style>
